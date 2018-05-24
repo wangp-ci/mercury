f36c0711f9ab5546704db9ef32398be9499226e3
Peter Wang <novalazy@gmail.com>
2018-05-24 15:07:45 +1000

    Initial attempt at Travis CI

---
+++ install_bootstrap_compiler.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -eux
+prefix=$1
+tarball=$2
+test -f "$tarball" || wget "http://dl.mercurylang.org/rotd/$tarball"
+tar xf "$tarball"
+cd "${tarball#.tar.gz}"
+./configure --prefix="$prefix" --with-default-grade=hlc.gc --enable-libgrades=hlc.gc
+make
+make install
