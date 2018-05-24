56e805ce8c1b65ba7d85edecdce994352b153853
Peter Wang <novalazy@gmail.com>
2018-05-24 16:00:44 +1000

    Strip suffix correctly.

--- install_bootstrap_compiler.sh
+++ install_bootstrap_compiler.sh
@@ -4,7 +4,7 @@ prefix=$1
 tarball=$2
 test -f "$tarball" || wget "http://dl.mercurylang.org/rotd/$tarball"
 tar xf "$tarball"
-cd "${tarball#.tar.gz}"
+cd "${tarball%.tar.gz}"
 ./configure --prefix="$prefix" --with-default-grade=hlc.gc --enable-libgrades=hlc.gc
 make
 make install
