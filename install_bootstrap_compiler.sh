9b1c1476dc437289b86241dbc99df51bdaffc011
Peter Wang <novalazy@gmail.com>
2018-05-24 21:04:00 +1000

    Try caching mercury-bootstrap, -j2, container-based; disable bootcheck for now.

--- install_bootstrap_compiler.sh
+++ install_bootstrap_compiler.sh
@@ -2,9 +2,12 @@
 set -eux
 prefix=$1
 tarball=$2
+if [[ -d "$prefix" ]] ; then
+    exit
+fi
 test -f "$tarball" || wget "http://dl.mercurylang.org/rotd/$tarball"
 tar xf "$tarball"
 cd "${tarball%.tar.gz}"
 ./configure --prefix="$prefix" --with-default-grade=hlc.gc --enable-libgrades=hlc.gc
-make
-make install
+make -j2
+make -j2 install
