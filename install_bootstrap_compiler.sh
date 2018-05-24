5f2b5d7e026854e4bee5d7dbc28e8e123cbf1a44
Peter Wang <novalazy@gmail.com>
2018-05-24 21:27:08 +1000

    Check bootstrap compiler version, make PARALLEL=-j2

--- install_bootstrap_compiler.sh
+++ install_bootstrap_compiler.sh
@@ -2,14 +2,14 @@
 set -eux
 prefix=$1
 tarball=$2
-# XXX change version
-if "$prefix/bin/mmc" --version | grep rotd-2018-05-23
+basename=${tarball%.tar.gz}
+version=${basename#mercury-srcdist-}
+if [[ -x "$prefix/bin/mmc" ]] && "$prefix/bin/mmc" --version | grep -F "version $version,"
 then
     exit
 fi
 test -f "$tarball" || wget "http://dl.mercurylang.org/rotd/$tarball"
 tar xf "$tarball"
-cd "${tarball%.tar.gz}"
+cd "$basename"
 ./configure --prefix="$prefix" --with-default-grade=hlc.gc --enable-libgrades=hlc.gc
-make -j2
-make -j2 install
+make PARALLEL=-j2 install
